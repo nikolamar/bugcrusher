e5c51fa7163102e2c6be2ffb991ecdd36983a1ed
nikolamar <nikolamar@gmail.com>
2021-08-10 00:59:35 +0200

    bottom toast

--- packages/client-react/src/components/toaster/styles.ts
+++ packages/client-react/src/components/toaster/styles.ts
@@ -5,7 +5,7 @@ export const Container = styled.div`
     width: 100%;
     z-index: 101;
     text-align: center;
-    top: 0px;
+    bottom: 0px;
     left: 0; right: 0;
     text-align: center;
 `;
