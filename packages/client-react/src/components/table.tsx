31470312edec7f665d5bd39a8dd0cd5c965ac270
nikolamar <nikolamar@gmail.com>
2021-08-17 13:45:41 +0200

    update table

--- packages/client-react/src/components/table.tsx
+++ packages/client-react/src/components/table.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
 const head = ['ID', 'Time', 'Type', 'Data'];
 const data = [
@@ -6,21 +6,49 @@ const data = [
     ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
     ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
     ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
+    ['Aaf23aa454w5wqlQIJJijdwijqid', '11/07/2021 13:54 am', 'console:log', 'sdfsafsjdfkjskfajk'],
 ];
 
 export function Table({}): React.ReactElement {
+
+    const [filter, setFilter] = React.useState('');
+
+    const handleFilterChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setFilter(e.currentTarget.value);
+    }
+
     return (
-        <table cellSpacing={0} className="bc-table">
-            <thead>
-                <tr>
-                    {head.map((el: string) => <th> <div>{el}</div></th>)}
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((record: any) =>
-                    <tr>{record.map((el: string, index: number) =>
-                        <td data-label={head[index]}>{el}</td>)}</tr>)}
-            </tbody>
-        </table>
+        <div className="bc-table">
+            <section>
+                <input type="text" placeholder="Search..." onChange={handleFilterChange} value={filter}/>
+            </section>
+            <table cellSpacing={0}>
+                <thead>
+                    <tr>
+                        {head.map((el: string, i: number) => <th key={i}> <div>{el}</div></th>)}
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((record: any, ri: number) =>
+                        <tr key={ri}>{record.map((el: string, ei: number) =>
+                            <td key={ei} data-label={head[ei]}>{el}</td>)}</tr>)}
+                </tbody>
+            </table>
+        </div>
     );
 }
