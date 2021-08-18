c9d90e598b858a38a21ea6219865ac0f40498c51
nikolamar <nikolamar@gmail.com>
2021-08-18 18:23:29 +0200

    button state to user

--- packages/client-react/src/hooks/create-state.ts
+++ packages/client-react/src/hooks/create-state.ts
@@ -4,7 +4,8 @@ export function createState(initial: string) {
     const [state, setState] = React.useState(initial);
 
     const handleClick = () => {
-        state === 'button' && setState('recording');
+        state === 'user' && setState('recording');
+        state === 'admin' && setState('report');
         state === 'recording' && setState('report');
     }
 
